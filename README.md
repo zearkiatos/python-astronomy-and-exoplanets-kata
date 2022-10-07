012b7e217daf7315b41040d7332ce29394d0bdee
zearkiatos <caprilespe@outlook.com>
2022-10-06 22:19:43 -0300

    Type of detection by year pie

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ This is a python 🐍 project kata 🥋 about how to show data of astronomy and 
 
 ![Github](https://github.com/zearkiatos/python-astronomy-and-exoplanets-kata/actions/workflows/action.yml/badge.svg)
 ![GitlabCI](https://gitlab.com/caprilespe/python-astronomy-and-exoplanets-kata/badges/develop/pipeline.svg)
+![TravisCI](https://api.travis-ci.com/zearkiatos/python-astronomy-and-exoplanets-kata.svg?branch=develop)
 
 # Make with
 [![Python](https://img.shields.io/badge/python-2b5b84?style=for-the-badge&logo=python&logoColor=white&labelColor=000000)]()
--- exoplanetas.py
+++ exoplanetas.py
@@ -7,7 +7,8 @@ import numpy as np
 
 plt.rcParams.update({'font.size': 12})
 
-def cargar_datos(nombre_archivo:str)->pd.DataFrame:
+
+def cargar_datos(nombre_archivo: str) -> pd.DataFrame:
     """ Carga los datos de un archivo csv y retorna el DataFrame con la informacion.
     Parametros:
         nombre_archivo (str): El nombre del archivo CSV que se debe cargar
@@ -19,14 +20,15 @@ def cargar_datos(nombre_archivo:str)->pd.DataFrame:
     return data
 
 
-def histograma_descubrimiento(datos:pd.DataFrame)->None:
+def histograma_descubrimiento(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un histograma con 30 grupos (bins) en el que debe
         aparecer la cantidad de planetas descubiertos por anho.
     Parametros:
         datos (DataFrame): el DataFrame con la informacion de los exoplanetas
     """
     discovery = datos["DESCUBRIMIENTO"]
-    ax = discovery.plot(kind="hist", bins=30, title="Cantidad de planetas descubiertos entre 1988 y 2018")
+    ax = discovery.plot(
+        kind="hist", bins=30, title="Cantidad de planetas descubiertos entre 1988 y 2018")
 
     ax.set_xlabel("Años", fontsize=9)
 
@@ -38,14 +40,16 @@ def histograma_descubrimiento(datos:pd.DataFrame)->None:
     plt.show()
     pass
 
-def estado_publicacion_por_descubrimiento(datos:pd.DataFrame)->None:
+
+def estado_publicacion_por_descubrimiento(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un BoxPlot donde aparecen la cantidad de planetas
         descubiertos por anho, agrupados de acuerdo con el tipo de publicacion.
     Parametros:
         datos (DataFrame): el DataFrame con la informacion de los exoplanetas
     """
-    
-    ax = datos[["DESCUBRIMIENTO", "ESTADO_PUBLICACION"]].boxplot(by="ESTADO_PUBLICACION", figsize=(9,30))
+
+    ax = datos[["DESCUBRIMIENTO", "ESTADO_PUBLICACION"]].boxplot(
+        by="ESTADO_PUBLICACION", figsize=(9, 30))
 
     ax.set_ylabel("Año de descubrimiento")
 
@@ -58,13 +62,15 @@ def estado_publicacion_por_descubrimiento(datos:pd.DataFrame)->None:
 
     pass
 
-def deteccion_por_descubrimiento(datos:pd.DataFrame)->None:
+
+def deteccion_por_descubrimiento(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un BoxPlot donde aparecen la cantidad de planetas
         descubiertos por anho, agrupados de acuerdo con el tipo de deteccion
     Parametros:
         datos (DataFrame): el DataFrame con la informacion de los exoplanetas
     """
-    ax = datos[["DESCUBRIMIENTO", "TIPO_DETECCION"]].boxplot(by="TIPO_DETECCION", figsize=(9,9))
+    ax = datos[["DESCUBRIMIENTO", "TIPO_DETECCION"]].boxplot(
+        by="TIPO_DETECCION", figsize=(9, 9))
 
     ax.set_ylabel("Año de descubrimiento")
 
@@ -76,9 +82,9 @@ def deteccion_por_descubrimiento(datos:pd.DataFrame)->None:
     plt.show()
 
     pass
-    pass
 
-def deteccion_y_descubrimiento(datos:pd.DataFrame,anho:int)->None:
+
+def deteccion_y_descubrimiento(datos: pd.DataFrame, anho: int) -> None:
     """ Calcula y despliega un diagrama de pie donde aparecen la cantidad de
         planetas descubiertos en un anho particular, clasificados de acuerdo
         con el tipo de publicacion.
@@ -88,9 +94,27 @@ def deteccion_y_descubrimiento(datos:pd.DataFrame,anho:int)->None:
         anho (int): el anho para el que se quieren analizar los planetas descubiertos
                     o 0 para indicar que deben ser todos los planetas.
     """
+    name = ''
+    if anho == 0:
+        data = datos[["DESCUBRIMIENTO", "TIPO_DETECCION"]
+                     ]
+        name = "pie-tipo-deteccion"
+    else:
+        data = datos[["DESCUBRIMIENTO", "TIPO_DETECCION"]]
+        data = data[data["DESCUBRIMIENTO"] == anho]
+        name = "pie-tipo-deteccion-"+str(anho)
+
+    data.groupby(["TIPO_DETECCION"]).sum().plot(
+        kind="pie", y="DESCUBRIMIENTO", autopct='%1.0f%%', legend=False)
+
+    plt.savefig("./assets/"+name+".png")
+
+    plt.show()
+
     pass
 
-def cantidad_y_tipo_deteccion(datos:pd.DataFrame)->None:
+
+def cantidad_y_tipo_deteccion(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un diagrama de lineas donde aparece una linea por
         cada tipo de deteccion y se muestra la cantidad de planetas descubiertos
         en cada anho, para ese tipo de deteccion.
@@ -100,7 +124,7 @@ def cantidad_y_tipo_deteccion(datos:pd.DataFrame)->None:
     pass
 
 
-def masa_promedio_y_tipo_deteccion(datos:pd.DataFrame)->None:
+def masa_promedio_y_tipo_deteccion(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un diagrama de lineas donde aparece una linea por
         cada tipo de detección y se muestra la masa promedio de los planetas descubiertos
         en cada anho, para ese tipo de deteccion.
@@ -110,7 +134,7 @@ def masa_promedio_y_tipo_deteccion(datos:pd.DataFrame)->None:
     pass
 
 
-def masa_planetas_vs_masa_estrellas(datos: pd.DataFrame)->None:
+def masa_planetas_vs_masa_estrellas(datos: pd.DataFrame) -> None:
     """ Calcula y despliega un diagrama de dispersión donde en el eje x se
         encuentra la masa de los planetas y en el eje y se encuentra el logaritmo
         de la masa de las estrellas. Cada punto en el diagrama correspondera
@@ -122,7 +146,7 @@ def masa_planetas_vs_masa_estrellas(datos: pd.DataFrame)->None:
     pass
 
 
-def graficar_cielo(datos:pd.DataFrame)->list:
+def graficar_cielo(datos: pd.DataFrame) -> list:
     """ Calcula y despliega una imagen donde aparece un pixel por cada planeta,
         usando colores diferentes que dependen del tipo de detección utilizado
         para descubirlo.
@@ -134,13 +158,10 @@ def graficar_cielo(datos:pd.DataFrame)->list:
     pass
 
 
-def filtrar_imagen_cielo(imagen:list)->None:
+def filtrar_imagen_cielo(imagen: list) -> None:
     """ Le aplica a la imagen un filtro de convolucion basado en la matriz
         [[-1,-1,-1],[-1,9,-1],[-1,-1,-1]]
     Parametros:
         imagen (list): una matriz con la imagen del cielo
     """
     pass
-
-
-
